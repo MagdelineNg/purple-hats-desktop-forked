19c1e3d1ff972d88e30e3c8bb1fcff3cb93d9e1b
magdeline <MNG045@e.ntu.edu.sg>
2023-06-16 13:02:57 +0800

    add inno compiler step into ps1

--- installer.ps1
+++ installer.ps1
@@ -24,13 +24,21 @@ Remove-Item -Path $FEdestinationPath
 
 echo "Purple HATS Frontend extracted to $FEextractPath."
 
-# Invoke-WebRequest -Uri $innoSetupCompilerUrl -OutFile $innoSetupCompilerPath
-
-# echo "InnoSetup compiler extracted to $innoSetupCompilerPath."
-
-# Start-Process "$env:APPDATA\iscc.exe" -ArgumentList "/LOG /O`"$env:APPDATA" `"C:\Program Files\Purple HATS Desktop\Purple HATS Frontend\hats_for_windows.iss`"" -Wait -NoNewWindow 
+# Invoke-WebRequest -Uri "https://jrsoftware.org/download.php/is.exe" -OutFile "$env:APPDATA\is.exe" -Verb runas
 
 #echo "Inno Setup compiler has been installed."
+
+#add env var 
+$env:path = "$env:path" + ";${env:programfiles(x86)}\Inno Setup 6"
+ISCC /O"${env:APPDATA}" "C:\Program Files (x86)\Inno Setup 6\Examples\Example1.iss"
+
+#ISCC /O "Purple-HATS Installer windows
+
+Start-Process "${env:ProgramFiles(x86)}\Inno Setup 6\ISCC.exe"  
+
+# Start-Process "${env:ProgramFiles(x86)}\Inno Setup 6\ISCC.exe" -ArgumentList "/LOG /O`"$env:APPDATA`" `"C:\Program Files\Purple HATS Desktop\Purple HATS Frontend\hats_for_windows.iss`"" -Wait 
+
+# echo "InnoSetup compiler extracted to $innoSetupCompilerPath."
 
 Move-Item -Path "$current_path\hats_for_windows.iss" -Destination "$env:APPDATA\hats_for_windows.iss" -Force
 
